428912bab41d986a979d93ba4119e8af46697984
Rudolf Hersén <rudolf.hersen@schibsted.se>
2016-10-28 07:55:39 +0200

    Move code from back to front

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-document.getElementById('root').insertAdjacentHTML('beforeend', '<h1>Hello ES2015!</h1>');
+require('./style.css')
+
+const getHtml = require('./getHtml')
+
+const r1 = new XMLHttpRequest()
+const r2 = new XMLHttpRequest()
+
+r1.open('GET', '/json/stations', true)
+r2.open('GET', '/json/current', true)
+
+let stations
+
+r1.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+        const trainStations = JSON.parse(this.response).RESPONSE.RESULT[0].TrainStation
+        stations = {}
+        trainStations.forEach(entry => stations[entry.LocationSignature] = entry.AdvertisedShortLocationName)
+        r2.send()
+    }
+}
+r2.onload = function () {
+    if (this.status >= 200 && this.status < 400)
+        document.getElementById('root')
+            .insertAdjacentHTML('beforeend',
+                getHtml(JSON.parse(this.response).RESPONSE.RESULT[0].TrainAnnouncement, stations))
+}
+
+r1.send()
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 module.exports = {
-  entry: './index',
-  output: {
-    filename: 'browser-bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
+    entry: './index',
+    output: {
+        filename: 'browser-bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            { test: /\.css$/, loader: 'style-loader!css-loader' }
+        ]
+    },
+    devServer: {
+        proxy: {
+            '/json': {
+                target: 'http://localhost:1339',
+                secure: false
+            }
         }
-      }
-    ]
-  }
-};
+    }
+}
